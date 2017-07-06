fb59b00c5c7ce62d4ff3d916358301a806597752
kwokbk <kwokbk@hawaii.edu>
2017-07-05 14:59:55 -1000

    done

---
+++ test/test.js
@@ -0,0 +1,63 @@
+const expect = require('chai').expect;
+const should = require('chai').should;
+
+describe('Person', function() {
+  let p = new Person('Ben', 25);
+
+  it('should have a name property, that is a string', function() {
+    expect(p).to.have.a.property('name');
+    expect(p.name).to.be.a('string');
+  });
+
+  it('should have an age property, that is a number', function() {
+    expect(p).to.have.a.property('age');
+    expect(p.age).to.be.a('number');
+  });
+
+  it("should have a sayHello() method that prints 'Hello!'",function() {
+    expect(doesLogMessage(p.sayHello, 'Hello!')).to.be.true;
+  });
+
+  it("should have a sayName() method that prints the Person's name: 'My name is 'name'", function() {
+    expect(doesLogMessage(p.sayName.bind(p), `My name is ${p.name}`)).to.be.true;
+  });
+
+  it("should have a sayAge() method that prints the Person's age: 'I am 'age' years old'", function() {
+    expect(doesLogMessage(p.sayAge.bind(p), `I am ${p.age} years old`)).to.be.true;
+  });
+});
+
+function doesLogMessage(f, message) {
+    var oldLog = console.log,
+        result = false;
+
+    console.log = function(s) {
+        if (s == message) {
+            result = true;
+        }
+    };
+
+    f();
+
+    console.log = oldLog;
+
+    return result;
+}
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+
+Person.prototype.sayHello = function() {
+  console.log('Hello!');
+}
+
+Person.prototype.sayName = function() {
+  console.log(`My name is ${this.name}`);
+}
+
+Person.prototype.sayAge = function() {
+  console.log(`I am ${this.age} years old`)
+}
